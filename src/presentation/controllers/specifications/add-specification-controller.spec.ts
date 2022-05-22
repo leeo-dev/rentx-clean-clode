03dbcef26510dc9e0e9dd480818a33a574fa8aea
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-05-22 10:41:45 -0300

    test: AddSpecificationController returns 400 if no name is provided

---
+++ src/presentation/controllers/specifications/add-specification-controller.spec.ts
@@ -0,0 +1,23 @@
+import { MissingParamError } from '@/presentation/error'
+import { badRequest } from '@/presentation/helpers/http-helper'
+import { AddSpecificationController } from './add-specification-controller'
+
+type SutTypes = {
+  sut: AddSpecificationController
+}
+const makeSut = (): SutTypes => {
+  const sut = new AddSpecificationController()
+  return { sut }
+}
+describe('Add Specification Controller', () => {
+  test('should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        description: 'any_description'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+})
