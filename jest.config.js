e2e8b22a08b03a36c5e45cb81d051bbfd0ded8bc
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-05-22 09:11:02 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': '@swc/jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
