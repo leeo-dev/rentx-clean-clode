2379ae901df92498c0986cfc51dfe39c03a72788
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-05 21:13:54 -0300

    feat: ensure DbAddSpecification returns an specification on loadByName Success

--- src/data/usecases/db-add-specification.ts
+++ src/data/usecases/db-add-specification.ts
@@ -5,7 +5,11 @@ import { LoadSpecificationByNameRepository } from '@/data/protocols/load-specifi
 export class DbAddSpecification implements AddSpecification {
   constructor (private readonly loadSpecificationByNameRepository: LoadSpecificationByNameRepository) {}
   async add ({ name, description }: SpecificationParam): Promise<Specification | null> {
-    await this.loadSpecificationByNameRepository.loadByName(name)
+    const specification = await this.loadSpecificationByNameRepository.loadByName(name)
+    console.log('-----------------------')
+    console.log(specification)
+    console.log('-----------------------')
+    if (specification) return specification
     return null
   }
 }
