a1bbbe3b519a57c6e9c0e92e1f7d303878b8d631
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-05 21:14:03 -0300

    test: ensure DbAddSpecification returns an specification on loadByName Success

--- __tests__/data/usecases/specifications/db-add-specification.spec.ts
+++ __tests__/data/usecases/specifications/db-add-specification.spec.ts
@@ -1,6 +1,8 @@
 import { DbAddSpecification } from '@/data/usecases/db-add-specification'
 import { LoadSpecificationByNameRepository } from '@/data/protocols/load-specification-by-name-repository'
 import { Specification } from '@/domain/models/specification'
+import mockDate from 'mockdate'
+import { mockSpecification } from '@/../__mocks__/mock-specification'
 
 const mockLoadSpecificationByNameRepository = (): LoadSpecificationByNameRepository => {
   class LoadSpecificationByNameRepositoryStub implements LoadSpecificationByNameRepository {
@@ -23,10 +25,22 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbAddSpecification UseCase', () => {
+  beforeAll(async () => {
+    mockDate.set(new Date())
+  })
+  afterAll(async () => {
+    mockDate.reset()
+  })
   test('Should call LoadSpecificationByNameRepository with correct name', async () => {
     const { sut, loadSpecificationByNameRepositoryStub } = makeSut()
     const loadByNameSpy = jest.spyOn(loadSpecificationByNameRepositoryStub, 'loadByName')
     await sut.add({ name: 'any_name', description: 'any_description' })
     expect(loadByNameSpy).toHaveBeenLastCalledWith('any_name')
+  })
+  test('Should return a specification if LoadSpecificationByNameRepository returns a specification', async () => {
+    const { sut, loadSpecificationByNameRepositoryStub } = makeSut()
+    jest.spyOn(loadSpecificationByNameRepositoryStub, 'loadByName').mockReturnValueOnce(Promise.resolve(mockSpecification()))
+    const specification = await sut.add({ name: 'any_name', description: 'any_description' })
+    expect(specification).toEqual(mockSpecification())
   })
 })
