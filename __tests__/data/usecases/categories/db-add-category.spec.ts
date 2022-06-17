7910b01e88bb75d5e745116a271095363fec8484
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-17 14:48:18 -0300

    test: ensure DbAddCategory returns a Category if already exists

--- __tests__/data/usecases/categories/db-add-category.spec.ts
+++ __tests__/data/usecases/categories/db-add-category.spec.ts
@@ -1,7 +1,8 @@
-import { mockCategoryParam } from '@/../__mocks__/mock-categories'
+import { mockCategory, mockCategoryParam } from '@/../__mocks__/mock-categories'
 import { LoadCategoryByNameRepository } from '@/data/protocols/load-category-by-name-repository'
 import { Category } from '@/domain/models/category'
 import { DbAddCategory } from '@/data/usecases/db-add-category'
+import mockDate from 'mockdate'
 
 const mockLoadCategoryByNameRepository = (): LoadCategoryByNameRepository => {
   class LoadCategoryByNameRepositoryStub implements LoadCategoryByNameRepository {
@@ -25,10 +26,22 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Db Add Category', () => {
+  beforeAll(async () => {
+    mockDate.set(new Date())
+  })
+  afterAll(async () => {
+    mockDate.reset()
+  })
   test('should call LoadCategoryByNameRepository with correct name', async () => {
     const { sut, loadCategoryByNameRepositoryStub } = makeSut()
     const loadByNameSpy = jest.spyOn(loadCategoryByNameRepositoryStub, 'loadByName')
     await sut.add(mockCategoryParam())
     expect(loadByNameSpy).toHaveBeenCalledWith(mockCategoryParam().name)
+  })
+  test('should return a Category if LoadCategoryByNameRepository return a category', async () => {
+    const { sut, loadCategoryByNameRepositoryStub } = makeSut()
+    jest.spyOn(loadCategoryByNameRepositoryStub, 'loadByName').mockReturnValueOnce(Promise.resolve(mockCategory()))
+    const result = await sut.add(mockCategoryParam())
+    expect(result).toEqual(mockCategory())
   })
 })
