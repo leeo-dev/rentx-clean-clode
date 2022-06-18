c71da8988511ceb60a43907c396899650a327e6c
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-18 15:06:22 -0300

    test: ensure Category Router return 400 if category already exists

--- __tests__/main/routes/category.test.ts
+++ __tests__/main/routes/category.test.ts
@@ -16,4 +16,12 @@ describe('Category Router', () => {
       .send({ name: 'any_name', description: 'any_description' })
       .expect(201)
   })
+  test('Should return 400 if category already exists', async () => {
+    const newSpecification = new CategoryMongo({ name: 'any_name', description: 'any_description' })
+    await newSpecification.save()
+    await request(app)
+      .post('/api/categories')
+      .send({ name: 'any_name', description: 'any_description' })
+      .expect(403)
+  })
 })
