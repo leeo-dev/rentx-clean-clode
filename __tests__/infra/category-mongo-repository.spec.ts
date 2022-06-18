6317a85da6e8b76332348bc531130cd2e5452705
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-18 14:48:42 -0300

    test: ensure CategoryMongoDb load a category on success

--- __tests__/infra/category-mongo-repository.spec.ts
+++ __tests__/infra/category-mongo-repository.spec.ts
@@ -12,11 +12,20 @@ describe('Category MongoRepository', () => {
   const makeSut = (): CategoryMongoDb => {
     return new CategoryMongoDb()
   }
-  test('should add a specification on success', async () => {
+  test('should add a category on success', async () => {
     const sut = makeSut()
     await sut.add({ name: 'any_name', description: 'any_description' })
     const spec = await CategoryMongo.findOne({ name: 'any_name' })
     expect(spec).toBeTruthy()
     expect(spec).toHaveProperty('_id')
+  })
+
+  test('should load a category on success', async () => {
+    const sut = makeSut()
+    const category = new CategoryMongo({ name: 'any_name', description: 'any_description' })
+    await category.save()
+    const categoryResult = await sut.loadByName('any_name')
+    expect(categoryResult).toBeTruthy()
+    expect(categoryResult).toHaveProperty('name')
   })
 })
