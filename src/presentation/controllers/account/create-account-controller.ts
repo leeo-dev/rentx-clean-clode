ea9927d40c596e46141217ed3376612aa6a2d7fd
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-19 16:25:31 -0300

    feat: ensure CreateAccountController returns 400 if is invalid email is provided

--- src/presentation/controllers/account/create-account-controller.ts
+++ src/presentation/controllers/account/create-account-controller.ts
@@ -1,4 +1,5 @@
 import { MissingParamError } from '@/presentation/errors'
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { badRequest } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
@@ -11,7 +12,8 @@ export class CreateAccountController implements Controller {
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
     }
     const { email } = httpRequest.body
-    await this.emailValidator.validate(email)
+    const isEmailValid = await this.emailValidator.validate(email)
+    if (!isEmailValid) return badRequest(new InvalidParamError('email'))
     return badRequest(new MissingParamError('name'))
   }
 }
