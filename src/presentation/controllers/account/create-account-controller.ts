12d9efd292e2ffafbae16ba8ca5d9ef9d012ffb1
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-19 11:53:47 -0300

    feat: ensure CreateAccountController returns 400 if no password is provided

--- src/presentation/controllers/account/create-account-controller.ts
+++ src/presentation/controllers/account/create-account-controller.ts
@@ -4,6 +4,10 @@ import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class CreateAccountController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['name', 'password', 'email', 'drive_license', 'avatar']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    }
     return badRequest(new MissingParamError('name'))
   }
 }
