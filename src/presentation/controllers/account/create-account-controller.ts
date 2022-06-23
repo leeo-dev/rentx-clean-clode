4639c3e46c8921dbb32c8cf50fcd050e8ca04ceb
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-06-23 20:54:17 -0300

    feat: ensure CreateAccountController returns 500 if DbCreateAccount throws

--- src/presentation/controllers/account/create-account-controller.ts
+++ src/presentation/controllers/account/create-account-controller.ts
@@ -1,7 +1,7 @@
 import { DbCreateAccount } from '@/domain/protocols/create-account'
 import { AlreadyInUseError, MissingParamError } from '@/presentation/errors'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
-import { badRequest } from '@/presentation/helpers/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
 
@@ -11,15 +11,19 @@ export class CreateAccountController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'password', 'email', 'driveLicense']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'password', 'email', 'driveLicense']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      }
+      const { name, email, driveLicense, password } = httpRequest.body
+      const isEmailValid = await this.emailValidator.validate(email)
+      if (!isEmailValid) return badRequest(new InvalidParamError('email'))
+      const isEmailAlreadyInUse = await this.dbCreateAccount.create({ name, email, driveLicense, password })
+      if (isEmailAlreadyInUse) return badRequest(new AlreadyInUseError('email'))
+      return badRequest(new MissingParamError('name'))
+    } catch (error: any) {
+      return serverError(error)
     }
-    const { name, email, driveLicense, password } = httpRequest.body
-    const isEmailValid = await this.emailValidator.validate(email)
-    if (!isEmailValid) return badRequest(new InvalidParamError('email'))
-    const isEmailAlreadyInUse = await this.dbCreateAccount.create({ name, email, driveLicense, password })
-    if (isEmailAlreadyInUse) return badRequest(new AlreadyInUseError('email'))
-    return badRequest(new MissingParamError('name'))
   }
 }
